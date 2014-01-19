08b4b8717e5bb8a58cdc53323f1925bd6a5e85a7
Woon Ket Wong <woonketwong@hotmail.com>
2014-01-18 16:22:00 -0800

    Hide all but first slide on it

--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -4,7 +4,12 @@ define(['backbone', 'views/slide'], function(Backbone, SlideView){
 
 		initialize: function(){
 			this.renderAll();
+			App.Vent.on('init', this.hideAllButFirst, this);
 		},
+
+		hideAllButFirst: function() {
+			this.$el.children(':nth-child(n+2)').hide();
+		}
 
 		renderAll: function(){
 			this.$el.empty();
