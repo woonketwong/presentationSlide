1fd92d5e1ad115f99e41b608a0150b6c33a7d5f0
Woon Ket Wong <woonketwong@hotmail.com>
2014-01-21 15:05:05 -0800

    commented code snippet feature

--- app/slides.js
+++ app/slides.js
@@ -1,12 +1,10 @@
 window.slides = [
-	{ title: 'My Presentation'},
 	{ title: 'Hello, my name is Woon Ket'},
-	{ title: 'I work at Hack Reactor'},
-	{ title: 'Some CSS Code Here',
-		snippet: 'snippets/ex1.css'
-  },
-	{ quote: 'Lorem ipsum dolor sdgg', cite: "Steve"},
-	{ title: 'dsafadsfdsafadsfasd dsafsadf sdaklmrtrt njansdlb dfagnjkdfga erjtnbknjndfg adbgajdgnesadfsa sdafsadfsdaf sdafasdfagfa mnk,adnsfd,f,m dsafasdf asdfadsfsadf sadfasdf sadffadsfn dsafbnjkadsfnjk sdfajfnjkasdfn sadfjnbg dafs;bnjkan sdafjbnasdjkfbnj sdafjnbjkgands jnasdjkfgnjkasndf asdfjnasjdgfnjkadsfngasd'},
-	{ title: 'Why this matters', bullets: ['Point One', 'Point Two', 'Point Three']},
-	{ image: 'http://placekitten.com/1500/1500'}
+	{ title: 'I am a software engineer specializing in full-stack Javascript development.'},
+	{ title: 'I have extensive experience with Backbone, Angular, and Node.'},
+	{ title: '...as well as with HTML 5 and CSS 3.'}
+	// { quote: 'Lorem ipsum dolor sdgg', cite: "Steve"},
+	// { title: 'dsafadsfdsafadsfasd dsafsadf sdaklmrtrt njansdlb dfagnjkdfga erjtnbknjndfg adbgajdgnesadfsa sdafsadfsdaf sdafasdfagfa mnk,adnsfd,f,m dsafasdf asdfadsfsadf sadfasdf sadffadsfn dsafbnjkadsfnjk sdfajfnjkasdfn sadfjnbg dafs;bnjkan sdafjbnasdjkfbnj sdafjnbjkgands jnasdjkfgnjkasndf asdfjnasjdgfnjkadsfngasd'},
+	// { title: 'Why this matters', bullets: ['Point One', 'Point Two', 'Point Three']},
+	// { image: 'http://placekitten.com/1500/1500'}
 ]
