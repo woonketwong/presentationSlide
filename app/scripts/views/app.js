4054dab1c84c1d0d08b45ef05a57b371f9d8b242
Woon Ket Wong <woonketwong@hotmail.com>
2014-01-20 18:57:02 -0800

    Add functionality to change slide

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -19,6 +19,21 @@ function(Backbone, SlidesView, SlidesCollection, MainRouter){
 
 			App.router = new MainRouter();
 			Backbone.history.start();
+		},
+
+		events: {
+			'keyup': 'keyUp'
+		},
+
+		keyUp: function(e) {
+			// 37 = left
+			// 39 = right
+			if (e.keyCode === 37 || e.keyCode === 39){
+				App.Vent.trigger('changeSlide', {
+					direction: e.keyCode === 39? 'next' : 'prev'
+				});
+			}
+			console.log(e);
 		}
 	});
 
--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -3,12 +3,82 @@ define(['backbone', 'views/slide'], function(Backbone, SlideView){
 		el: $('.slides'),
 
 		initialize: function(){
+			this.currentSlideIndex = 1;
+			this.numSlides = this.collection.length;
+			this.transitionSpeed = 400;
+
 			this.renderAll();
 			App.Vent.on('init', this.hideAllButFirst, this);
+			App.Vent.on('changeSlide', this.changeSlide, this);
 		},
 
 		hideAllButFirst: function() {
 			this.$el.children(':nth-child(n+2)').hide();
+		},
+
+		changeSlide: function(opts) {
+			var self = this;
+			var newSlide;
+			var slides = this.$el.children();
+
+			this.setCurrentSlideIndex(opts);
+
+			newSlide = this.getNextSlide(slides);
+
+			this.animateToNewSlide(slides, newSlide, opts.direction);
+
+			App.router.navigate('/slides/' + this.currentSlideIndex);
+
+		},
+
+		setCurrentSlideIndex: function(opts) {
+			// If we're requesting a special slide
+			// then set current index
+			if (opts.slideIndex ){
+				return this.currentSlideIndex = ~~opts.slideIndex;
+			}
+
+			// Otherwise, just grab the next or prev slide
+			// this.setCurrentSlideIndex(opts.direction);
+
+			this.currentSlideIndex += opts.direction === 'next' ? 1 : -1;
+			console.log(this.currentSlideIndex);
+
+			if (this.currentSlideIndex > this.numSlides){
+				// Go back to slide number 1
+				this.currentSlideIndex = 1;
+			}
+
+			if (this.currentSlideIndex <= 0){
+				// Go back to slide number 1
+				this.currentSlideIndex = this.numSlides;
+			}
+
+		},
+
+		getNextSlide: function(slides) {
+			return slides.eq(this.currentSlideIndex - 1);
+
+		},
+
+		animateToNewSlide: function(slides, newSlide, direction){			
+			slides.filter(':visible')
+			.css('position', 'absolute') // Temporary
+				.animate({
+					top: direction === 'next' ? '100%' : '-100%',
+					opacity: 'hide'
+				}, this.transitionSpeed, function(){
+					// slide is gone from view
+					$(this).css('top', 0);
+
+					// bring new slide into view
+					newSlide
+						.css('top', direction === 'next' ? '-100%' : '100%')
+						.animate({
+							top: 0,
+							opacity: 'show'
+						}, self.transitionSpeed);
+				});
 		},
 
 		renderAll: function(){
