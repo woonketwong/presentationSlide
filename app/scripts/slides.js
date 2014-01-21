0a96473c28673b2e46e9d8ef67d6f80184933292
Woon Ket Wong <woonketwong@hotmail.com>
2014-01-21 12:34:07 -0800

    Add renders for bullets and headings

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -3,5 +3,6 @@ window.slides = [
 	{ title: 'Hello, my name is Woon Ket'},
 	{ title: 'I work at Hack Reactor'},
 	{ title: 'dsafadsfdsafadsfasd dsafsadf sdaklmrtrt njansdlb dfagnjkdfga erjtnbknjndfg adbgajdgnesadfsa sdafsadfsdaf sdafasdfagfa mnk,adnsfd,f,m dsafasdf asdfadsfsadf sadfasdf sadffadsfn dsafbnjkadsfnjk sdfajfnjkasdfn sadfjnbg dafs;bnjkan sdafjbnasdjkfbnj sdafjnbjkgands jnasdjkfgnjkasndf asdfjnasjdgfnjkadsfngasd'},
+	{ title: 'Why this matters', bullets: ['Point One', 'Point Two', 'Point Three']},
 	{ image: 'http://placekitten.com/1500/1500'}
 ]
--- app/scripts/views/slide.js
+++ app/scripts/views/slide.js
@@ -5,11 +5,12 @@ define(['backbone'], function(Backbone){
 		render: function(){
 			if (this.model.get('image')){
 				this.renderImage();
+			} else if (this.model.get('bullets')){
+				this.renderBullets();
+			} else {
+				this.renderHeading();
 			}
 
-			this.$el.append(
-				'<h1 class=' + this.model.get('size') + '>' + this.model.get('title') + '</h1>'
-				);
 			return this;
 		},
 
@@ -17,7 +18,30 @@ define(['backbone'], function(Backbone){
 			this.$el
 				.addClass('image')
 				.append('<img src="' + this.model.get('image') + '">');
+		},
+
+		renderBullets: function(){
+			var el = this.$el;
+
+			if (this.model.get('title') ){
+				this.$el.append('<h1>' + this.model.get('title') + '</h1>');
+			}
+
+			el
+				.addClass('bullets')
+				.append([
+					'<ul>',
+						'<li>' + this.model.get('bullets').join('</li><li>'),
+					'</ul>'
+				].join(''));
+		},
+
+		renderHeading: function(){
+			this.$el.append(
+				'<h1 class=' + this.model.get('size') + '>' + this.model.get('title') + '</h1>'
+			);
 		}
+
 	});
 
 	return Slide;
